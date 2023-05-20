19819fe17dd20ab7862a46d252dcb512ac5444c0
SanUlrich <SanUlrich@mail.ru>
2023-05-20 22:43:42 +0200

    create homepage func, ArticleListView and ArticleDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Article
+
+
+def homepage(request):
+    """
+    Returns simple home page.
+    """
+    return render(request, "blog/homepage.html")
+
+
+class ArticleListView(generic.ListView):
+    """
+    Displays list of all articles with pagination.
+    """
+    model = Article
+    paginate_by = 10
+    template_name = "blog/article_list.html"
+    context_object_name = "articles"
+
+
+class ArticleDetailView(generic.DetailView):
+    """
+    Displays detailed page of particular article.
+    """
+    model = Article
+    template_name = "blog/article_detail.html"
