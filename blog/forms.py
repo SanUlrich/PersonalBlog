135f4a2bcca078f6ceee8ff80ac7beb56d46ad63
SanUlrich <SanUlrich@mail.ru>
2023-05-24 13:41:05 +0200

    The __init__ method has been removed as it is no longer needed

--- blog/forms.py
+++ blog/forms.py
@@ -9,21 +9,3 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initiates form's fields with bootstrap style.
-        """
-        super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Username"}
-        )
-        self.fields["email"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Email address"}
-        )
-        self.fields["password1"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Password"}
-        )
-        self.fields["password2"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Repeat password"}
-        )
