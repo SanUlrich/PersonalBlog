06c87ad6b7b01c0fde71824222813e67008b5d2c
SanUlrich <SanUlrich@mail.ru>
2023-05-27 13:54:42 +0200

    Add "like" and "dislike" fields for Article model

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
     """
     Article model. Include title, intro, main text and publication/update date.
     """
-    article_title = models.CharField("Article title", max_length=255, db_index=True)
-    intro = models.TextField("Article intro", blank=True)
-    text = models.TextField("Article text")
+    author = models.ForeignKey(User, related_name="articles", on_delete=models.CASCADE)
+    article_title = models.CharField("Article title", max_length=255, unique=True, db_index=True)
+    intro = models.TextField("Article intro", blank=True, db_index=True)
+    text = models.TextField("Article text", db_index=True)
     publication_date = models.DateField("Publication date", auto_now_add=True)
     update_date = models.DateField("Update date", auto_now=True)
     slug = models.SlugField("URL", max_length=255, unique=True, db_index=True)
+    like = models.PositiveIntegerField("Likes", default=0)
+    dislike = models.PositiveIntegerField("Dislikes", default=0)
 
     def __str__(self):
         return self.article_title
@@ -22,8 +26,11 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Comments model.
+    """
     article = models.ForeignKey(Article, related_name="comments", on_delete=models.CASCADE)
-    comment_text = models.TextField(help_text="Comment")
+    comment_text = models.TextField(max_length=255, help_text="Comment")
     username = models.CharField(max_length=255, help_text="Username")
     comment_date = models.DateTimeField(auto_now_add=True)
 
