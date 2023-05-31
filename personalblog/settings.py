80f082b43b1f5a79c47f285b0f443516ad682cd9
SanUlrich <SanUlrich@mail.ru>
2023-05-31 14:33:04 +0200

    Added redirect links
    Added LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL

--- personalblog/settings.py
+++ personalblog/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect links
+
+LOGIN_REDIRECT_URL = 'blog:article-list'
+LOGOUT_REDIRECT_URL = 'blog:login'
