651ea7e4f8f7aca0cbd111225e9c06199cbb544b
SanUlrich <SanUlrich@mail.ru>
2023-05-31 13:11:19 +0200

    Added CreateNewArticleForm
    Form for creating a new article.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+
+from .models import Article
 
 
 class RegisterUserForm(UserCreationForm):
@@ -9,3 +12,9 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class CreateNewArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ["article_title", "intro", "text"]
